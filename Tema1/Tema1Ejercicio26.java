25a8ee21cc7629b10d1a225acc03e317718e595a
4ndr33400 <andrmontal@gmail.com>
2023-10-19 10:46:28 +0200

    rahhhhh

--- Tema1/Tema1Ejercicio26.java
+++ Tema1/Tema1Ejercicio26.java
@@ -2,21 +2,11 @@ import java.util.Scanner;
 public class Tema1Ejercicio26 {
    public static void main (String [] args){
        Scanner wa = new Scanner(System.in);
-
-
        System.out.println("Introduce un numero para saber si es primo o no");
-
-
        int N = wa.nextInt();
        int contador = 2;
        boolean primo = true;
-
-
-
-
        while ((primo) && contador != N) {
-
-
            if (N % contador == 0)
                primo = false;
            contador++;
--- Tema1/Tema1Ejercicio30.java
+++ Tema1/Tema1Ejercicio30.java
@@ -5,37 +5,20 @@ public class Tema1Ejercicio30 {
        Scanner in = new Scanner (System.in);
        char seguir;
 
-
-
-
-
-
-
-
        do {
            double A,B,sol;
            char signo;
 
-
            System.out.println("Escribe un valor");
            A = in.nextFloat();
-
 
            System.out.println("Elije que operacion deseas realizar : \n + \n - \n * \n / \n %");
            signo = in.next().charAt(0);
 
-
            System.out.println("Escribe otro valor");
            B = in.nextDouble();
 
-
-
-
-
-
            switch (signo) {
-
-
                case '+':
                    sol = A + B;
                    System.out.println("El resultado a la suma es " + sol);

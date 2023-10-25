b7f35e168fbe4d0559d99fb443ab6cfba847cb67
4ndr33400 <andrmontal@gmail.com>
2023-10-25 13:56:00 +0200

    ejercicio8

--- Tema2/Tema2Ejercicio8.java
+++ Tema2/Tema2Ejercicio8.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 public class Tema2Ejercicio8 {
-    public static boolean Dias_Mes (int mes,int dias){
+    public static boolean Dias_Mes (int mes,int dias, int año){
         if (mes > 0 && mes < 13) {
             if (mes == 1 || mes == 3 || mes ==  5 || mes== 7 || mes == 8 || mes == 10 || mes == 12 && dias <= 31){
-                System.out.println ("El dia y el mes son correctos");
+                System.out.println ("La fecha es correcta");
             } else if (mes == 2 ||mes == 4 ||mes == 6 ||mes == 9 ||mes == 11 && dias <= 30){
-                System.out.println("El dia y el mes son correctos");
+                if ((año % 4 == 0 && mes==2 && dias <= 29) || (mes == 2 && dias == 28)){
+                    System.out.println("La fecha es correcta");
+                    return true;
+                }
+                System.out.println("La fecha es correcta");
+                return true;
             }
         } else {
+            System.out.println("NO");
             return false;
-        }
-        return true;
-    }
-    public static boolean Año (int año){
-        año = 0;
-        int dias =0;
-        int mes =0;
-        if (año % 2 == 0 && mes==2 && dias <= 29){
-            System.out.println("El año es correcto");
-        }
-        return true;
+        }return true;
     }
     public static void main (String [] args){
         Scanner guagua = new Scanner (System.in);
@@ -34,8 +30,6 @@ public class Tema2Ejercicio8 {
         System.out.print("Introduce el año: ");
         año=guagua.nextInt();
 
-        Dias_Mes(dias,mes);
-        Año(año);
-
+        Dias_Mes(mes,dias,año);
     }
 }

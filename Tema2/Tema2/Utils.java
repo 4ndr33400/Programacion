13ffbdaaad04e785fcc896907e3347bd84ef91c4
4ndr33400 <andrmontal@gmail.com>
2023-11-09 09:48:23 +0100

    UtilsTest

--- Tema2/Tema2/Utils.java
+++ Tema2/Tema2/Utils.java
@@ -1,7 +1,5 @@
 package Tema2;
 
-import java.sql.SQLOutput;
-
 public class Utils{
 
     public static double miles2kilometers (double get_miles){
@@ -52,7 +50,89 @@ public class Utils{
         return amount_coin;
     }
     public static int getNIF (int numberNIF){
-
+        int result = 0;
+        numberNIF = numberNIF % 23;
+    switch (numberNIF){
+        case 0:
+            result = 'T';
+            break;
+        case 1:
+            result = 'R';
+            break;
+        case 2:
+            result = 'W';
+            break;
+        case 3:
+            result = 'A';
+            break;
+        case 4:
+            result = 'G';
+            break;
+        case 5:
+            result = 'M';
+            break;
+        case 6:
+            result = 'Y';
+            break;
+        case 7:
+            result = 'F';
+            break;
+        case 8:
+            result = 'P';
+            break;
+        case 9:
+            result = 'D';
+            break;
+        case 10:
+            result = 'X';
+            break;
+        case 11:
+            result = 'B';
+            break;
+        case 12:
+            result = 'N';
+            break;
+        case 13:
+            result = 'J';
+            break;
+        case 14:
+            result = 'Z';
+            break;
+        case 15:
+            result = 'S';
+            break;
+        case 16:
+            result = 'Q';
+            break;
+        case 17:
+            result = 'V';
+            break;
+        case 18:
+            result = 'H';
+            break;
+        case 19:
+            result = 'L';
+            break;
+        case 20:
+            result = 'C';
+            break;
+        case 21:
+            result = 'K';
+            break;
+        case 22:
+            result = 'E';
+            break;
+    }
+        return result = Character.toUpperCase(result);
     }
 
-}
+        public static boolean isValidNIF (int getNIF ,char getLetter){
+        boolean result = true;
+        getLetter  = Character.toUpperCase(getLetter);
+            if (getNIF(getNIF) != getLetter) {
+                result = false;
+            }
+            return result;
+        }
+    }
+

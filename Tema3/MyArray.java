07ffc3434ae8ed2fa95d0975d30010384fd19f36
4ndr33400 <andrmontal@gmail.com>
2023-11-16 20:22:36 +0100

    Arrays

---
+++ Tema3/MyArray.java
@@ -0,0 +1,15 @@
+public class MyArray {
+    public static void ArrayNumberPrint(int [] ArrayNumber) {
+        for (int i = 0; i < ArrayNumber.length; i++) {
+            System.out.println(ArrayNumber[i]);
+        }
+    }
+    public static void ArrayGreatestNumberPrint(int [] Number){
+
+    }
+    public static void main(String[] args) {
+        int ArrayNumber [] = {1,2,3};
+        ArrayNumberPrint(ArrayNumber);
+    }
+}
+

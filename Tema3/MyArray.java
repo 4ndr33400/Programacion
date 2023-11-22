b10e3131f7b24010d500e042fe3faeb064f50a25
4ndr33400 <andrmontal@gmail.com>
2023-11-22 13:56:56 +0100

    Arrays

--- Tema3/MyArray.java
+++ Tema3/MyArray.java
@@ -3,21 +3,12 @@ import java.util.Scanner;
 public class MyArray {
     static Scanner in = new Scanner(System.in);
 
-    public static int[] CreateArray(int[] MyArray) {
-        System.out.println("Introduzca 5 numeros a continuacion :");
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Numero " + i + " = ");
-            int number = in.nextInt();
-            MyArray[i] = number;
+    public static void ArrayNumberPrint(int[] Array) {
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i]);
         }
-        return MyArray;
     }
 
-    public static void ArrayNumberPrint(int[] ArrayNumber) {
-        for (int i = 0; i < ArrayNumber.length; i++) {
-            System.out.println(ArrayNumber[i]);
-        }
-    }
     public static int ArrayGreatestNumber(int[] Array) {
         int Maximo = Array[0];
         for (int i = 1; i < Maximo; i++) {
@@ -27,25 +18,36 @@ public class MyArray {
         }
         return Maximo;
     }
-    public static int ArrayMinimumNumber(int[] Array){
+
+    public static int ArrayMinimumNumber(int[] Array) {
         int Minimo = Array[0];
-        for (int i = 0; i > Minimo; i++){
-            if (i < Minimo){
-                Array[i]=Minimo;
+        for (int i = 0; i > Minimo; i++) {
+            if (i < Minimo) {
+                Array[i] = Minimo;
             }
         }
         return Minimo;
     }
-    public static void main(String[] args) {
 
-        int ArrayNumber[] = new int[]{0};
-        CreateArray(ArrayNumber);
-        ArrayGreatestNumber(CreateArray(ArrayNumber));
-        ArrayMinimumNumber(CreateArray(ArrayNumber));
-        /*
-        ArrayNumberPrint(ArrayNumber);
-        ArrayGreatestNumber(CreateArray(ArrayNumber));
-         */
+    public static int ArrayAverage(int[] Array) {
+        int average = Array[0];
+        for (int i = 0; i < 5; i++) {
+            Array[i] += average;
+        }
+        return average / 5;
+    }
+
+    public static void main(String[] args) {
+        int MyArray[] = new int[5];
+        System.out.println("Introduzca 5 numeros a continuacion :");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Numero " + i + " = ");
+            int number = in.nextInt();
+            MyArray[i] = number;
+        }
+        System.out.println("El mayor es: " + ArrayGreatestNumber(MyArray));
+        System.out.println("El menor es: " + ArrayMinimumNumber(MyArray));
+        System.out.println("La media del array es: " + ArrayAverage(MyArray));
     }
 }
 

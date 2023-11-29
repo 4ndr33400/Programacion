c712221f6c9e5f4925f7e22660b04066da650b64
4ndr33400 <andrmontal@gmail.com>
2023-11-29 18:04:25 +0100

    AtrapaLaMosca

--- Tema3/AtrapaLaMosca.java
+++ Tema3/AtrapaLaMosca.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 public class AtrapaLaMosca {
     static Random random = new Random();
+    static Scanner in =new Scanner(System.in);
     public static int[] flyPositionCreate(){
         int [] returnArrayFly = new int[15];
         int length = 15;
@@ -15,6 +17,36 @@ public class AtrapaLaMosca {
         }
         return returnArrayFly;
     }
-    public static int
+    public static void CachTheFly (int[] Array){
+        System.out.println("Donde crees que esta la mosca? ");
+        int eleccion = in.nextInt();
+        int[] FlyElection = flyPositionCreate();
+        int count = 0;
+        do {
+            for (int i = 0; i < FlyElection.length; i++) {
+                if (eleccion == FlyElection[i]) {
+                    System.out.println("Le has dado a la mosca :D ya puedes dormir en paz ijijijijjiji");
+                    count++;
+                    break;
+                } else if ((eleccion + 1) == FlyElection[i]) {
+                    System.out.println("Estuviste a punto de matar a la mosca");
+                    flyPositionCreate();
+                } else if (eleccion != FlyElection[i]) {
+                    System.out.println("Vuelve a intentarlo");
+                }
+            }
+        }while (count <= 0);
+    }
+    public static void main(String[] args){
+        System.out.println("Bienvenido/a al juego de la mosca " +
+                "\n En este juego tendras que elegir un numero de entre el 0 y el 15, si el numero que elejiste esta cerca de la mosca esta se movera de posicion, " +
+                "y si no lo esta la mosca permanecera en su sitio. Buena suerte!");
+        System.out.print("Introduce next para continuar :");
+        String next = in.nextLine();
 
+        Arrays.toString(flyPositionCreate());
+        int[] FlyArray = flyPositionCreate();
+        CachTheFly(FlyArray);
+
+    }
 }

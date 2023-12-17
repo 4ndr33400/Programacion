1f1e2cd9c12cec1d92e20a27188bbc8f62f01cdf
4ndr33400 <andrmontal@gmail.com>
2023-12-17 18:12:28 +0100

    hundirLaFlotaSinTerminarXD

--- Tema3/HundirLaFlota.java
+++ Tema3/HundirLaFlota.java
@@ -1,3 +1,28 @@
 public class HundirLaFlota {
+    static int filas = 10;
+    static int columnas = 10;
+    static char[][] tablero;
+    public static void main(String[] args) {
 
+
+    }
+    public static char[][] crearTablero (){
+        tablero = new char[filas][columnas];
+        for(int x = 0; x < filas; x++){
+            for (int y = 0; y < columnas; y++){
+                tablero[x][y] = '-';
+            }
+        }
+        return tablero;
+    }
+    public static void imprimirTablero(char[][] tablero){
+        System.out.println(" A B C D E F G H I J");
+        for (int x = 0; x < filas ; x++){
+            System.out.println(x + 1 + " ");
+            for (int y = 0; y < columnas ; y++){
+                System.out.println(tablero[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
 }

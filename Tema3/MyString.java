14f2d63d57aa19ab3e870d3468d9e0e031faf056
4ndr33400 <andrmontal@gmail.com>
2024-01-10 19:41:34 +0100

    ejercicios b y c de strings

--- Tema3/MyString.java
+++ Tema3/MyString.java
@@ -3,39 +3,58 @@ import java.util.Scanner;
 public class MyString {
     static String introducedString;
     static Scanner in = new Scanner(System.in);
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         System.out.println("Seleccione a continuacion una de las siguientes opciones :");
         System.out.println("(a) Devolver cadena invertida y en mayusculas" +
-                "\n(b) Contar las vocales de la cadena");
+                "\n(b) Contar las vocales de la cadena" +
+                "\n(c) Cual es la palabra mas larga de la cadena ");
         System.out.print("Introduzca la seleccion :");
         String option = in.nextLine();
-        switch(option){
+        System.out.println("Introduce la cadena: ");
+        switch (option) {
             case "a":
-                System.out.println("Introduce la cadena: ");
-                introducedString = in.next();
+                introducedString = in.nextLine();
                 System.out.println(invertedMayus(introducedString));
                 break;
             case "b":
-                System.out.println("Introduce la cadena: ");
-                introducedString = in.next();
+                introducedString = in.nextLine();
                 System.out.println("Hay " + vowelCount(introducedString) + " vocales");
+                break;
+            case "c":
+                introducedString = in.nextLine();
+                System.out.println("La palabra mas larga es : " + greatestLength(introducedString) );
                 break;
         }
     }
-     public static String invertedMayus (String string1){
+
+    public static String invertedMayus(String string1) {
         String invertedString = "";
-        for(int i = string1.length() - 1; i >= 0; i--){
-           invertedString = invertedString + string1.charAt(i);
+        for (int i = string1.length() - 1; i >= 0; i--) {
+            invertedString = invertedString + string1.charAt(i);
         }
         return invertedString.toUpperCase();
     }
-    public static int vowelCount (String string2){
+
+    public static int vowelCount(String string2) {
         int vowelCounter = 0;
-        for(int i = 0 ; i < string2.length() ; i++){
-            if (string2.charAt(i) == 'a' || string2.charAt(i) == 'e'|| string2.charAt(i) == 'i'|| string2.charAt(i) == 'o'|| string2.charAt(i) == 'u'){
-                vowelCounter ++;
+        for (int i = 0; i < string2.length(); i++) {
+            if (string2.charAt(i) == 'a' || string2.charAt(i) == 'e' || string2.charAt(i) == 'i' || string2.charAt(i) == 'o' || string2.charAt(i) == 'u') {
+                vowelCounter++;
             }
         }
         return vowelCounter;
     }
+
+    public static String greatestLength(String string3) {
+        String[] words = string3.split(" ");
+        String greatestWord = "";
+        for (String word : words) {
+            if (word.length() > greatestWord.length()) {
+                greatestWord = word;
+            }
+        }
+        return greatestWord;
+    }
+
 }

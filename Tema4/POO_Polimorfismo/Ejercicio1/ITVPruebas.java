b455df3ca115c7c52644104f03ef75c11855b702
4ndr33400 <andrmontal@gmail.com>
2024-02-15 20:18:01 +0100

    adelantar polimorfismo

--- Tema4/POO_Polimorfismo/Ejercicio1/ITVPruebas.java
+++ Tema4/POO_Polimorfismo/Ejercicio1/ITVPruebas.java
@@ -5,25 +5,52 @@ import java.util.Scanner;
 public class ITVPruebas {
     static Scanner in = new Scanner(System.in);
     static Random random = new Random();
-    public static void main(String[] args) {
-        int numeroVehiculos = 1;
+    static String licensePlate;
 
-      Vehicle[] vehicles = new Vehicle[numeroVehiculos];
+    public static void main(String[] args) {
+        int numeroVehiculos = 0;
+
+      Vehicle[] vehicles = new Vehicle[5];
       do{
           System.out.println("Ha entrado un nuevo vehiculo");
-          int vehicleType = random.nextInt();
-          switch (vehicleType){
-              case 1:
-                  System.out.println("Ha entrado un Turismo: ");
-                  vehicles[numeroVehiculos] = new Turism();
-                  System.out.println("Cual es el numero de matricula?: ");
-                  String licensePlate = in.nextLine();
-              case 2:
-
+          System.out.print("Si lo quiere recibir introduzca next: ");
+          String option = in.nextLine().toLowerCase();
+          if(option.equals("next")){
+              int vehicleType = random.nextInt(1, 3) + 1;
+              switch (vehicleType) {
+                  case 1:
+                      System.out.println("Ha entrado un Turismo: ");
+                      System.out.println("Cual es el numero de matricula?: ");
+                      licensePlate = in.nextLine();
+                      vehicles[numeroVehiculos] = new Turism();
+                      break;
+                  case 2:
+                      System.out.println("Ha entrado un Camion");
+                      System.out.println("Cual es el numero de matricula?: ");
+                      licensePlate = in.nextLine();
+                      vehicles[numeroVehiculos] = new Truck();
+                      break;
+                  case 3:
+                      System.out.println("Ha entrado una moto");
+                      System.out.println("Cual es el numero de matricula?: ");
+                      licensePlate = in.nextLine();
+                      vehicles[numeroVehiculos] = new Cyclomotor();
+                      break;
+              }
+              numeroVehiculos++;
+          } else if (!option.equals("next") || numeroVehiculos <= 5){
+              for (int i = 0; i < vehicles.length; i++){
+                  Vehicle vehicle = vehicles[i];
+                  System.out.println("Coche numero " + i);
+                  System.out.println(vehicle);
+              }
+              System.out.println("Adioh");
+              break;
           }
 
-          numeroVehiculos++;
-      }while();
+      }while(numeroVehiculos <= 5);
+
+
 
     }
 }
--- Tema4/POO_Polimorfismo/Ejercicio1/Vehicle.java
+++ Tema4/POO_Polimorfismo/Ejercicio1/Vehicle.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public  class Vehicle {
     public String licensePlate;
     public int incomeMoment;
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
     public  void checkVehicle(){
         System.out.println("Se ha revisado el vehiculo");
     }
@@ -12,7 +17,7 @@ public  class Vehicle {
         System.out.println("El vehiculo ha entrado a la hora");
     }
     public  String toString(){
-        return "Matricula: " + licensePlate +
-                "Hora de llegada: " + incomeMoment;
+        return "Matricula: " + getLicensePlate() +
+                "\nHora de llegada: " + incomeMoment;
     }
 }
